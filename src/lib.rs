4f8510345ef09fb891661ddb38014c329cce1492
PhilVoel <philipp123.voeller@gmail.com>
2023-04-01 04:54:01 +0200

    Added function to get file content and return String (or error)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn get_file_content(file_name: &String) -> Result<String, std::io::Error>{
+   std::fs::read_to_string(file_name) 
 }
