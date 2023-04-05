2a5f818a964121e2b091a7396b18069268abe077
PhilVoel <philipp123.voeller@gmail.com>
2023-04-05 19:39:37 +0200

    Added basic functionality for parsing and getting values
    Works for strings, integers and floating point numbers

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # PML - Philipp's Modern Language
+
 Just another format to specify your configs in\
-Currently under development, so don't expect full functionality until version 1.0.0
+Currently under development, so don't expect full functionality until version 1.0.0\
+Create Struct with ̀`let pml = pml::parse_file("file_name")` and get values with `pml.get_int("key")`, `pml.get_float("key")` or `pml.get_string("key")` respectively.\
+Save the data in `file_name.pml` in the format
+
+```
+key1=TestString
+key2=1.41
+key3=69
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
-pub fn get_file_content(file_name: &str) -> Result<String, std::io::Error> {
-   std::fs::read_to_string(file_name) 
+use std::fs;
+use std::collections::HashMap;
+
+pub struct PmlStruct {
+    elements: HashMap<String, PmlElem>
+}
+
+enum PmlElem {
+    PmlString(String),
+    PmlInt(i32),
+    PmlFloat(f32)
+}
+
+impl PmlStruct {
+    pub fn get_string(&self, key: &str) -> &str {
+        match self.elements.get(key) {
+            Some(PmlElem::PmlString(s)) => s,
+            _ => panic!("Not a string")
+        }
+    }
+
+    pub fn get_int(&self, key: &str) -> &i32 {
+        match self.elements.get(key) {
+            Some(PmlElem::PmlInt(i)) => i,
+            _ => panic!("Not an int")
+        }
+    }
+
+    pub fn get_float(&self, key: &str) -> &f32 {
+        match self.elements.get(key) {
+            Some(PmlElem::PmlFloat(f)) => f,
+            _ => panic!("Not a float")
+        }
+    }
+}
+
+pub fn parse_file(file: &str) -> PmlStruct {
+    let lines = get_lines(String::from(file) + ".pml");
+    parse_lines(lines)
+}
+
+fn get_lines(file: String) -> Vec<String> {
+    let mut lines = Vec::new();
+    let contents = fs::read_to_string(file).unwrap();
+    for line in contents.lines() {
+        lines.push(line.to_string());
+    }
+    lines
+}
+
+fn parse_lines(lines: Vec<String>) -> PmlStruct {
+    let mut elements: HashMap<String, PmlElem> = HashMap::new();
+    for line in lines {
+        let (key, value) = line.split_once("=").unwrap();
+        if let Ok(num) = value.parse::<i32>() {
+            elements.insert(key.to_string(), PmlElem::PmlInt(num));
+        } else if let Ok(num) = value.parse::<f32>() {
+            elements.insert(key.to_string(), PmlElem::PmlFloat(num));
+        } else {
+            elements.insert(key.to_string(), PmlElem::PmlString(value.to_string()));
+        }
+    }
+    PmlStruct {elements}
 }
