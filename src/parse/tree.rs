9f61b836441a69892fb3541ef8245bfcae1bd1ed
PhilVoel <philipp123.voeller@gmail.com>
2023-12-22 06:26:59 +0100

    temp

--- src/parse/tree.rs
+++ src/parse/tree.rs
@@ -1,5 +1,5 @@
 use crate::ParseError;
-use std::{collections::HashMap, rc::{Rc, Weak}};
+use std::{str::Chars, collections::HashMap, rc::{Rc, Weak}};
 
 pub struct ParseTree {
     root: Rc<Node>,
@@ -26,80 +26,106 @@ enum ParseStatus {
     Value,
 }
 
+struct ParseState<'a> {
+    chars: Chars<'a>,
+    line: u32,
+    col: u32,
+}
+
 use ParseStatus::{Start, MetaInfo, QuotedKey, UnquotedKey, AfterKey, AfterEquals, Value};
 
 impl TryFrom<&str> for ParseTree {
     type Error = ParseError;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let mut status = Start;
-        let mut current = String::new();
-        let mut meta_infos = Vec::new();
-        let mut key = String::new();
+        let mut chars = value.chars();
         let mut line = 1;
-        let mut col = 1;
-        for char in value.chars() {
-            if char == '\n' {
-                line += 1;
-                col = 1;
-            } else {
-                col += 1;
-            }
-            match status {
-                Start if char.is_whitespace() => continue,
-                Start if char == '#' => status = MetaInfo,
-                Start if char == '"' => status = QuotedKey,
-                Start if is_char_reserved(char) => return Err(ParseError::IllegalCharacter{char, line, col}),
-                Start => {
-                    current.push(char);
-                    status = UnquotedKey;
-                }
+        let mut col = 0;
 
-                MetaInfo if char == ';' => {
-                    meta_infos.push(current);
-                    current = String::new();
-                    status = Start;
-                }
-                MetaInfo => current.push(char),
 
-                QuotedKey if char == '"' => {
-                    key = current;
-                    current = String::new();
-                    status = AfterKey;
-                }
-                QuotedKey if char == ' ' => current.push(char),
-                QuotedKey if is_char_reserved(char) => return Err(ParseError::IllegalCharacter{char, line, col}),
-                QuotedKey => current.push(char),
-
-                UnquotedKey if char.is_whitespace() => {
-                    key = current;
-                    current = String::new();
-                    status = AfterKey;
-                }
-                UnquotedKey if char == '=' => {
-                    key = current;
-                    current = String::new();
-                    status = AfterEquals;
-                }
-                UnquotedKey if is_char_reserved(char) => return Err(ParseError::IllegalCharacter{char, line, col}),
-                UnquotedKey => current.push(char),
-
-                AfterKey if char.is_whitespace() => continue,
-                AfterKey if char == '=' => status = AfterEquals,
-                AfterKey => return Err(ParseError::IllegalCharacter{char, line, col}),
-
-                AfterEquals if char.is_whitespace() => continue,
-                AfterEquals if char == ';' => return Err(ParseError::IllegalCharacter{char, line, col}),
-                AfterEquals => {
-                    current.push(char);
-                    status = Value;
-                }
-
-                Value => todo!(),
-            }
+        let mut parse_state = ParseState {
+            chars,
+            line,
+            col,
+        };
+        let root = get_struct(&mut parse_state)?;
+        if let Some(char) = parse_state.chars.next() {
+            return Err(ParseError::IllegalCharacter{char, line: parse_state.line, col: parse_state.col});
         }
-        Err(ParseError::UnexpectedEOF)
     }
+}
+
+fn get_struct(parse_state: &mut ParseState) -> Result<Node, ParseError> {
+    let mut status = Start;
+    let mut current = String::new();
+    let mut meta_infos = Vec::new();
+    let mut key = String::new();
+    while let Some(char) = parse_state.chars.next() {
+        if char == '\n' {
+            parse_state.line += 1;
+            parse_state.col = 1;
+        } else {
+            parse_state.col += 1;
+        }
+        let line = parse_state.line;
+        let col = parse_state.col;
+        match status {
+            Start if char.is_whitespace() => continue,
+            Start if char == '#' => status = MetaInfo,
+            Start if char == '"' => status = QuotedKey,
+            Start if is_char_reserved(char) => return Err(ParseError::IllegalCharacter{char, line, col}),
+            Start => {
+                current.push(char);
+                status = UnquotedKey;
+            }
+
+            MetaInfo if char == ';' => {
+                meta_infos.push(current);
+                current = String::new();
+                status = Start;
+            }
+            MetaInfo => current.push(char),
+
+            QuotedKey if char == '"' => {
+                key = current;
+                current = String::new();
+                status = AfterKey;
+            }
+            QuotedKey if char == ' ' => current.push(char),
+            QuotedKey if is_char_reserved(char) => return Err(ParseError::IllegalCharacter{char, line, col}),
+            QuotedKey => current.push(char),
+
+            UnquotedKey if char.is_whitespace() => {
+                key = current;
+                current = String::new();
+                status = AfterKey;
+            }
+            UnquotedKey if char == '=' => {
+                key = current;
+                current = String::new();
+                status = AfterEquals;
+            }
+            UnquotedKey if is_char_reserved(char) => return Err(ParseError::IllegalCharacter{char, line, col}),
+            UnquotedKey => current.push(char),
+
+            AfterKey if char.is_whitespace() => continue,
+            AfterKey if char == '=' => status = AfterEquals,
+            AfterKey => return Err(ParseError::IllegalCharacter{char, line, col}),
+
+            AfterEquals if char.is_whitespace() => continue,
+            AfterEquals if char == ';' => return Err(ParseError::IllegalCharacter{char, line, col}),
+            AfterEquals if char == '{' => {
+                todo!()
+            }
+            AfterEquals => {
+                current.push(char);
+                status = Value;
+            }
+
+            Value => todo!(),
+        }
+    }
+    Err(ParseError::UnexpectedEOF)
 }
 
 fn is_char_reserved(char: char) -> bool {
